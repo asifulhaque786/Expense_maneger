e18169dff799c9f4c95fd9bb01e0e4780b047aee
asifulhaque786 <54080737+asifulhaque786@users.noreply.github.com>
2020-03-31 18:30:18 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,10 @@
-//api docs me search karo kya hai widget tumhare kaam ka
 
-//MaterialApp helps int switching the pages navigation
-//scaffold give the background appbar
-//layout me below
-/*container helps in wrapping up a widget helps int aligning etc
-rows ---- can have different child widgets
-column
--
--
--
-flexible and expanded how much space this child widget should consume
-stack widget one upon another
- card widget is container having default settings 
- listview abstract column that is scollable
- grid veiw combination of coloumn  nd row scollable
- textfield to take input
- gesturdetector
- inkwell event listners*/ ////\
 import 'package:flutter/cupertino.dart';
 import "package:flutter/material.dart";
 import 'package:new_project/user_transaction.dart';
 import 'package:rflutter_alert/rflutter_alert.dart';
 
-//import 'package:fluttertoast/fluttertoast.dart';
 
-//import 'package:new_project/transcation.dart';
-
-//there is built in feature to formate a state so extra package to extract and format it
-//pub.dev is a site where there are many packages to can install in dart packages
-//indentation are very important in yml file
-
-//$ use backslash to escape it
-//gives access to all not private classes.
 void main() => runApp(new MaterialApp(home: ExpenseApp()));
 
 class ExpenseApp extends StatefulWidget {
@@ -40,7 +13,7 @@ class ExpenseApp extends StatefulWidget {
 }
 
 class _ExpenseAppState extends State<ExpenseApp> {
-  // Container totalkarkede(BuildContext ctx){showModalBottomSheet(context:ctx , builder:(_){return GestureDetector(behavior:HitTestBehavior.opaque,child: bottamsheet());});}
+  // Container (BuildContext ctx){showModalBottomSheet(context:ctx , builder:(_){return GestureDetector(behavior:HitTestBehavior.opaque,child: bottamsheet());});}
   var nextpage = false;
 
   void alerts(BuildContext ctx) {
@@ -94,14 +67,7 @@ class _ExpenseAppState extends State<ExpenseApp> {
                 )
               ],
             ),
-            //column has an list of widgets
-            //text takes a as much it need size
-            //while card takes space as much of its child unless it has a parent which specifies the space
-            //coloumn has a width of child.default is infinity
-            //function on column has space between,space around
-            //elevation  is the shadow
-            //coloumn has two axis
-            //stech is used to fill all space
+            
             body: nextpage == false
                 ? SingleChildScrollView(
                     child: Column(
